5e1b1381062b87cee1d70231b415c3387d512865
xxx3105 <123968288+xxx3105@users.noreply.github.com>
2023-06-18 15:14:11 +0200

    18/06/23 task 1

--- js/02-video.js
+++ js/02-video.js
@@ -1,1 +1,16 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+
+const iframeEl = document.querySelector("iframe");
+const player = new Player(iframeEl);
+
+player.on('timeupdate', throttle(onPlay,1000));
+
+function onPlay(date) {
+    localStorage.setItem("videoplayer-current-time", date.seconds)
+};
+
+const item = localStorage.getItem("videoplayer-current-time")
+player.setCurrentTime(item);
+
 
--- js/03-feedback.js
+++ js/03-feedback.js
@@ -1,1 +1,2 @@
 
+//8 aufgabe 6 module
